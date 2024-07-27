dbf40c327469baeaa48b2fc451c9a8364123234f
WasiKhanayana <64360927+WasiKhanayana@users.noreply.github.com>
2024-07-28 02:27:22 +0500

    Following tutorial DONE

--- lib/models/category_model.dart
+++ lib/models/category_model.dart
@@ -10,7 +10,8 @@ class CategoryModel {
     required this.iconPath,
     required this.boxColor,
   });
-  List<CategoryModel> getCategories() {
+
+  static List<CategoryModel> getCategories() {
     List<CategoryModel> categories = [];
 
     categories.add(CategoryModel(
@@ -24,7 +25,7 @@ class CategoryModel {
       iconPath: 'assets/icons/pancakes.svg',
       boxColor: Color(0xffC588F2),
     ));
-    
+
     categories.add(CategoryModel(
       name: 'Pie',
       iconPath: 'assets/icons/pie.svg',
